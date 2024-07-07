30818a6f215842f5e72ee8e676278c4da719f89e
Husni <husniwfayo@gmail.com>
2024-07-07 19:40:43 +0530

    updated cors

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,39 @@
 import express from "express";
-const { connectDB } = require('./infrastructure/db'); 
+import cors, { CorsOptions, CorsRequest } from 'cors'; // Import types from cors
+const { connectDB } = require('./infrastructure/db');
 import jobsRouter from "./api/jobs";
 import jobApplicationRouter from "./api/job-application";
 import GlobalErrorHandler from "./api/middleware/global-error-handler";
 const dotenv = require('dotenv');
 
-
 dotenv.config();
 
 console.log('MONGO_URI:', process.env.MONGO_URI);
 
-
 const app = express();
 app.use(express.json());
-const cors = require('cors');
 
-app.use(cors());
+const allowedOrigins = ['http://localhost:5173', 'https://aidf-front-end-husni.netlify.app'];
+
+// Define the CORS options
+const corsOptions: CorsOptions = {
+  origin: function (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+
+app.use(cors(corsOptions));
 
 connectDB();
 
-app.use("/jobs", jobsRouter)
-app.use("/jobApplications", jobApplicationRouter)
+app.use("/jobs", jobsRouter);
+app.use("/jobApplications", jobApplicationRouter);
 
-app.use(GlobalErrorHandler)
+app.use(GlobalErrorHandler);
 
 const PORT = process.env.PORT || 5000;
 
